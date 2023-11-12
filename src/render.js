036d763ff9ca1d2736731228765fc00800f22575
KrystianRusin <krystian.r@hotmail.com>
2023-11-12 18:37:11 -0500

    cleander render.js

--- src/render.js
+++ src/render.js
@@ -4,35 +4,7 @@ const renderTodoList = (todoList) => {
     const todoF = todoFactory();
     const container = document.querySelector(".main-content")
     todoList.forEach(todo => {
-        const todoContainer = document.createElement("div")
-        todoContainer.classList.add("todo-container")
-
-        const todoTitle = document.createElement("h3")
-        todoTitle.innerHTML = todoF.getTitle(todo)
-        todoTitle.classList.add("todo-title")
-        todoContainer.appendChild(todoTitle)
-
-        const todoDesc = document.createElement("p")
-        todoDesc.innerHTML = todoF.getDesc(todo)
-        todoDesc.classList.add("todo-desc")
-        todoContainer.appendChild(todoDesc)
-
-        const todoDue = document.createElement("p")
-        todoDue.innerHTML = todoF.getDue(todo)
-        todoDue.classList.add("todo-due")
-        todoContainer.appendChild(todoDue)
-
-        const todoPrio = document.createElement("p")
-        todoPrio.innerHTML = todoF.getPrio(todo)
-        todoPrio.classList.add("todo-prio")
-        todoContainer.appendChild(todoPrio)
-
-        const todoDone = document.createElement("p")
-        todoDone.innerHTML = todoF.getIsDone(todo)
-        todoDone.classList.add("todo-done")
-        todoContainer.appendChild(todoDone)
-
-        container.appendChild(todoContainer)
+       renderTodo(todo)
     });
 }
 
