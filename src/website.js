f590bde865d42473fe5697aa66b611b0224f11a2
KrystianRusin <krystian.r@hotmail.com>
2023-11-12 18:35:28 -0500

    added add todo functionality

--- src/website.js
+++ src/website.js
@@ -1,4 +1,4 @@
-import { renderTodo } from "./render"
+import { renderTodoList, renderTodo } from "./render"
 import { todoFactory } from "./todo";
 
 const initWebsite = () => {
@@ -11,7 +11,17 @@ const initWebsite = () => {
     addTask.addEventListener("click", function () {
         addDialog.showModal()
     })
-    renderTodo(todoList)
+    renderTodoList(todoList)
+
+    const taskSubmit = document.getElementById("add-task-submit")
+    taskSubmit.addEventListener("click", function (event) {
+        event.preventDefault()
+        let title = document.getElementById("title").value
+        let desc = document.getElementById("desc").value
+        let date = document.getElementById("date").value
+        let prio = document.getElementById("priority").value
+        renderTodo(todo.createTodo(title, desc, date, prio, false))
+    })
 
 
 
